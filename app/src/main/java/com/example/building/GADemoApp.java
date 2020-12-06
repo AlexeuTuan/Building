ac031976a79596e44c2b6b39801911bac443366a
AlexeyTyan <alekseytyan45@gmail.com>
2020-12-06 17:36:53 +0300

    Global tracking

---
+++ app/src/main/java/com/example/building/GADemoApp.java
@@ -0,0 +1,12 @@
+package com.example.building;
+
+import android.app.Application;
+
+public class GADemoApp extends Application {
+    private String webId = "UA-NNNNNNNN-Y";
+    private Tracker tracker;
+
+    public synchronized Tracker getTracker() {
+
+    }
+}
